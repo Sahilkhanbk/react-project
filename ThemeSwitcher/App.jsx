1b260528e933fb1bef3d56c1d02ceef00df453f9
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-11-14 13:56:30 +0530

    Update App.jsx

--- ThemeSwitcher/App.jsx
+++ ThemeSwitcher/App.jsx
@@ -5,6 +5,9 @@ import { ThemeProvider } from './Context/Theme'
 import ThemeBtn from './components/ThemeBtn'
 import Card from './components/Card'
 
+
+
+
 function App() {
   const [themeMode, setthemeMode] = useState("light")
 
